f5eca8de4a3306e827c16633853249246aeda4df
Kevin Ligonde <Gardy.Ligonde@tufts.edu>
2014-02-25 16:45:32 -0500

    Removed the image source (spritesheet) from the html page and did everything on the game.js file instead

--- duckhunt/game.js
+++ duckhunt/game.js
@@ -2,8 +2,8 @@
 function draw() {
     canvas = document.getElementById('game');
     sprite = document.getElementById('dh');
-
-
+    sprite = new Image();
+    
     // Check if canvas is supported on browser
     if (canvas.getContext) {
         ctx = canvas.getContext('2d');
@@ -11,22 +11,25 @@ function draw() {
         ctx.fillRect (0, 0, 800, 520);
         ctx.fillStyle = "#C96A1B";
         ctx.fillRect (0, 520, 800, 80);
-        
-        // Bushes
-        ctx.drawImage(sprite,0,270,80,125,0,90,216,338);
 
-        // Tree 
-        ctx.drawImage(sprite,0,713,900,189,0,355,800,168);
+        sprite.onload = function() {
+            // Bushes
+            ctx.drawImage(sprite,0,270,80,125,0,90,216,338);
 
-        // Sniffing dog
-        ctx.drawImage(sprite,0,0,60,45,350,400,132,99);
+            // Tree 
+            ctx.drawImage(sprite,0,713,900,189,0,355,800,168);
 
-        // Birds
-        ctx.drawImage(sprite,0,110,40,35,220,50,100,75);
-        ctx.drawImage(sprite,125,110,40,35,320,220,100,75);
-        ctx.drawImage(sprite,255,110,40,35,420,100,100,75);
-        ctx.drawImage(sprite,0,150,40,35,600,50,100,75);
-        ctx.drawImage(sprite,125,150,40,35,50,10,100,75);	
+            // Sniffing dog
+            ctx.drawImage(sprite,0,0,60,45,350,400,132,99);
+
+            // Birds
+            ctx.drawImage(sprite,0,110,40,35,220,50,100,75);
+            ctx.drawImage(sprite,125,110,40,35,320,220,100,75);
+            ctx.drawImage(sprite,255,110,40,35,420,100,100,75);
+            ctx.drawImage(sprite,0,150,40,35,600,50,100,75);
+            ctx.drawImage(sprite,125,150,40,35,50,10,100,75);
+        }
+        sprite.src = "assets/duckhunt.png";	
     }
     else {
         alert('Sorry, canvas is not supported on your browser!');
