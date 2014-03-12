641e956a5eaefda7cf93f38b9442a5e0784e0118
Kevin Ligonde <Gardy.Ligonde@tufts.edu>
2014-03-12 19:50:19 -0400

    got rid of a few more erros. Still trying to get webpage to display map

--- transit/mapping.js
+++ transit/mapping.js
@@ -28,7 +28,7 @@ function initialize() {
         var map = new google.maps.Map(document.getElementById("map-canvas"),
             mapOptions);
       }
-     google.maps.event.addDomListener(window, 'load', initialize);
+     // google.maps.event.addDomListener(window, 'load', initialize);
 /*
 function loadJSON(path, success, error) {
     var xhr = new XMLHttpRequest();
