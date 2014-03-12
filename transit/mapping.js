46176118e1a4eee31a2cc9d05194fa85b0c1471a
Kevin Ligonde <Gardy.Ligonde@tufts.edu>
2014-03-12 18:49:46 -0400

    got rid of a few minor errors. Still in intial phase of testing

--- transit/mapping.js
+++ transit/mapping.js
@@ -1,3 +1,15 @@
+function initialize() {
+    getMyLocation();
+
+    var mapOptions = {
+          center: new google.maps.LatLng(lat, lng),
+          zoom: 8
+        };
+        var map = new google.maps.Map(document.getElementById("map-canvas"),
+            mapOptions);
+}
+    google.maps.event.addDomListener(window, 'load', initialize);
+
 function getMyLocation() {
     lat = -99999;
     lng = -99999;
@@ -14,19 +26,6 @@ function getMyLocation() {
     else {
         alert("Geolocation is not supported by your web browser =/");
     }
-}
-
-function initialize() {
-    getMyLocation();
-
-    var mapOptions = {
-          center: new google.maps.LatLng(lat, lng),
-          zoom: 8
-        };
-        var map = new google.maps.Map(document.getElementById("map-canvas"),
-            mapOptions);
-      }
-     google.maps.event.addDomListener(window, 'load', initialize);
 }
 
 function loadJSON(path, success, error) {
