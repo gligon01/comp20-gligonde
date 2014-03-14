069e4841e52db93a20fe18bd3bb2a939ccf4ad63
Kevin Ligonde <Gardy.Ligonde@tufts.edu>
2014-03-13 20:05:12 -0400

    Adding new code that hopefully will let map render on website correctly

--- transit/mapping.js
+++ transit/mapping.js
@@ -33,7 +33,7 @@ function getMyLocation() {
 }
 
 function initialize() {
-  var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+  var map = new google.maps.Map(document.getElementById("map-canvas"), myOptions);
   getMyLocation();
 }
      // from google website -> google.maps.event.addDomListener(window, 'load', initialize);
