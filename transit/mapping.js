e9ffa2b71d3c77d415b7fec159769610fe297a50
Kevin Ligonde <Gardy.Ligonde@tufts.edu>
2014-03-14 21:37:35 -0400

    Still trying to set up table

--- transit/mapping.js
+++ transit/mapping.js
@@ -160,7 +160,7 @@ function drawLines() {
 
   var img = {
     // This marker is 20 pixels wide by 32 pixels tall.
-    size: new google.maps.Size(5, 8),
+    size: new google.maps.Size(5, 8)
     // The origin for this image is 0,0.
     //origin: new google.maps.Point(0,0),
     // The anchor for this image is the base of the flagpole at 0,32.
