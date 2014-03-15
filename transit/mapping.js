9bcc877d070054961ec71c85fea5272504543f88
Kevin Ligonde <Gardy.Ligonde@tufts.edu>
2014-03-15 07:00:44 -0400

    Still working on tables

--- transit/mapping.js
+++ transit/mapping.js
@@ -255,7 +255,7 @@ function secondsToHms(d) {
   var h = Math.floor(d / 3600);
   var m = Math.floor(d % 3600 / 60);
   var s = Math.floor(d % 3600 % 60);
-  return ((h < 10 ? "0" + h + ":" : "") + (m > 0 ? (h > 0 && m < 10 ? "0" : "") + m + ":" : "0:") + (s < 10 ? "0" : "") + s); 
+  return ((h < 10 ? "0" : "")+ h + ":" + (m < 10 ? "0" : "") + m + ":" + (s < 10 ? "0" : "") + s); 
 }
 
 /*
