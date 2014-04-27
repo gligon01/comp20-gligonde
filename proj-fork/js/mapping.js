116adc362d68d9db2bc70319bb61dfb91e41ea9b
Kevin Ligonde <Gardy.Ligonde@tufts.edu>
2014-04-27 13:46:07 -0400

    trying to link multiple pages

--- proj-fork/js/mapping.js
+++ proj-fork/js/mapping.js
@@ -36,7 +36,6 @@ function initialize() {
     GEvent.addListener(gDir, "load", function() {
         var drivingDistanceMiles = gDir.getDistance().meters / 1609.344;
         var drivingDistanceKilometers = gDir.getDistance().meters / 1000;
-        document.getElementById('results').innerHTML = '<strong>Address 1: </strong>' + location1.address + ' (' + location1.lat + ':' + location1.lon + ')<br /><strong>Address 2: </strong>' + location2.address + ' (' + location2.lat + ':' + location2.lon + ')<br /><strong>Driving Distance: </strong>' + drivingDistanceMiles + ' miles (or ' + drivingDistanceKilometers + ' kilometers)';
     });
 }
 
